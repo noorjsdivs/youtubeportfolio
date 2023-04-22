d56243a60a47de48fdef10a1609f9ecf433f323e
Eslam Saadat (Iman) <80543205+eslamsaadat@users.noreply.github.com>
2023-04-22 22:02:25 +0430

    features was updated

---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe} from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
+
+// Features Data
+export const featuresData = [
+  {
+    id: 1,
+    title: "Business Stratagy",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 2,
+    icon: <AiFillAppstore />,
+    title: "App Development",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 3,
+    icon: <SiProgress />,
+    title: "SEO Optimisation",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 4,
+    icon: <FaMobile />,
+    title: "Mobile Development",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 5,
+    icon: <SiAntdesign />,
+    title: "UX Design",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 6,
+    icon: <FaGlobe />,
+    title: "Hosting Websites",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+];
