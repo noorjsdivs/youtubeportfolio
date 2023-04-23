fc8994e7a2a2f1fc253a1a8039e614e6f3d4b103
Hemanth Pothu <56197063+Hemanthpothu@users.noreply.github.com>
2023-04-23 17:39:11 +0530

    Added

--- src/components/footer/FooterBottom.js
+++ src/components/footer/FooterBottom.js
@@ -4,7 +4,7 @@ const FooterBottom = () => {
   return (
     <div className="w-full py-10">
       <p className="text-center text-gray-500 text-base">
-        © 2022. All rights reserved by Noor Mohammad
+        © 2022. All rights reserved by IIT BHU Students
       </p>
     </div>
   );
