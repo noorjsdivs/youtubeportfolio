ceb3af10ef6251954ff409b908c346681ba26d2c
Eslam Saadat (Iman) <80543205+eslamsaadat@users.noreply.github.com>
2023-04-27 01:36:31 +0430

    Banner was updated

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useTypewriter, Cursor } from "react-simple-typewriter";
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
-import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+import Media from './Media';
 
 const LeftBanner = () => {
     const [text] = useTypewriter({
@@ -32,43 +31,8 @@ const LeftBanner = () => {
           just to spruce things up, but doing it in ways that.
         </p>
       </div>
-      <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
-        <div>
-          <h2 className="text-base uppercase font-titleFont mb-4">
-            Find me in
-          </h2>
-          <div className="flex gap-4">
-            <span className="bannerIcon">
-              <FaFacebookF />
-            </span>
-            <span className="bannerIcon">
-              <FaTwitter />
-            </span>
-            <span className="bannerIcon">
-              <FaLinkedinIn />
-            </span>
-          </div>
-        </div>
-        <div>
-          <h2 className="text-base uppercase font-titleFont mb-4">
-            BEST SKILL ON
-          </h2>
-          <div className="flex gap-4">
-            <span className="bannerIcon">
-              <FaReact />
-            </span>
-            <span className="bannerIcon">
-              <SiNextdotjs />
-            </span>
-            <span className="bannerIcon">
-              <SiTailwindcss />
-            </span>
-            <span className="bannerIcon">
-              <SiFigma />
-            </span>
-          </div>
-        </div>
-      </div>
+     {/* Media */}
+     <Media />
     </div>
   );
 }
---
+++ src/components/banner/Media.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
+import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+const Media = () => {
+  return (
+    <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            Find me in
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaFacebookF />
+            </span>
+            <span className="bannerIcon">
+              <FaTwitter />
+            </span>
+            <span className="bannerIcon">
+              <FaLinkedinIn />
+            </span>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            BEST SKILL ON
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaReact />
+            </span>
+            <span className="bannerIcon">
+              <SiNextdotjs />
+            </span>
+            <span className="bannerIcon">
+              <SiTailwindcss />
+            </span>
+            <span className="bannerIcon">
+              <SiFigma />
+            </span>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Media
