0d73e9c5037d226c3800742825400f274495345b
Iman Saadat <e.saadat72@gmail.com>
2023-05-29 01:10:32 +0330

    Projects was updated

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 import { AiFillAppstore } from "react-icons/ai";
 import { FaMobile, FaGlobe} from "react-icons/fa";
 import { SiProgress, SiAntdesign } from "react-icons/si";
-
+import { projectOne, projectTwo, projectThree } from "../assets/index";
 // Features Data
 export const featuresData = [
   {
@@ -40,3 +40,43 @@ export const featuresData = [
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
   },
 ];
+
+// Projects Data  
+export const projectsData = [
+  {
+    id:1,
+    title : "SOCIAL MEDIA CLONE",
+    des : "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!",
+    img : projectOne
+  },
+  {
+    id:2,
+    title : "E-commerce Website",
+    des : "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!",
+    img : projectTwo
+  },
+  {
+    id:3,
+    title : "Chatting App",
+    des : "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!",
+    img : projectThree
+  },
+  {
+    id:4,
+    title : "SOCIAL MEDIA CLONE",
+    des : "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!",
+    img : projectOne
+  },
+  {
+    id:5,
+    title : "E-commerce Website",
+    des : "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!",
+    img : projectTwo
+  },
+  {
+    id:6,
+    title : "Chatting App",
+    des : "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!",
+    img : projectThree,
+  },
+]
