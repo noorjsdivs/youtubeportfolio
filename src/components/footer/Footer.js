7cd5fb068b2759fa8fcf1322ccb3bfde0bb4ba87
Iman Saadat <e.saadat72@gmail.com>
2023-05-31 19:39:25 +0330

    footer logo was updated

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="w-full py-20 h-auto border-b-[1px] border-b-black grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-4 gap-8">
       <div className="w-full h-full flex flex-col gap-8">
-        <Logo name="john doe" />
+        <Logo name="Mohammad" />
         <div className="flex gap-4">
           <span className="bannerIcon">
             <FaFacebookF />
